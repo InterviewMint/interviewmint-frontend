dd2d3458efb23bcc50b0cdec1e2336e2bc1f9609
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2025-11-21 19:25:31 +0530

    feat: integrate Tailwind CSS and update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            This is InterviewMint Frontend Application
+          </h1>
+          <p className="text-gray-600">
+            Welcome to your interview preparation platform
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
+  const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [
+      tailwindcss(),
       react({
         babel: {
-          plugins: [['babel-plugin-react-compiler']],
+          plugins: [["babel-plugin-react-compiler"]],
         },
       }),
     ],
     server: {
       port: Number(env.VITE_PORT) || 8080,
     },
-  }
-})
+  };
+});
